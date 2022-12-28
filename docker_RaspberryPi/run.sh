2b7e22a2ccd484dffbb91a3d36ba31937f1d68bd
Hiroaki Masuzawa <masuzawa.hiroaki.vl@tut.jp>
2022-12-28 14:04:17 +0900

    add use ft232h as usb-i2c bridge

--- docker_RaspberryPi/run.sh
+++ docker_RaspberryPi/run.sh
@@ -5,4 +5,4 @@ else
 CONFIGFILE='all_sensor.json'
 fi
 BUS_ID='1'
-sudo docker run --rm --name sensor_pi --device /dev/gpiomem  --device /dev/i2c-1 --privileged -e TZ=Asia/Tokyo -it -v `pwd`/../catkin_ws:/catkin_ws -w /catkin_ws sensor_pi bash -c "catkin_make; source /catkin_ws/devel/setup.bash; roslaunch sensor_pi sensor_pi.launch config_path:=\`rospack find sensor_pi\`/config/$CONFIGFILE bus_id:=$BUS_ID""
+sudo docker run --rm --name sensor_pi --device /dev/gpiomem  --device /dev/i2c-1 --privileged -e TZ=Asia/Tokyo -it -v `pwd`/../catkin_ws:/catkin_ws -w /catkin_ws sensor_pi bash -c "catkin_make; source /catkin_ws/devel/setup.bash; roslaunch sensor_pi sensor_pi.launch config_path:=\`rospack find sensor_pi\`/config/$CONFIGFILE bus_id:=$BUS_ID bus_type:=smbus"
